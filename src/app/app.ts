9aa2d984c08978371ce2844ec86e9e8848687f86
Lê Tự Huỳnh Duy <huynhduy20042002@gmail.com>
2025-09-23 11:37:08 +0700

    Refactor header layout and add Prettier config
    Moved header component from shared to layouts, splitting it into separate files for CSS, HTML, model, and spec. Added new layout files and updated references throughout the app. Introduced .prettierrc and VSCode settings for consistent code formatting, and updated dependencies to include Prettier and related plugins.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Simple-Angular-Example');
---
+++ src/app/layouts/header/header.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { NavLink } from './header.model';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  navLinks: NavLink[] = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Contact', path: '/contact' },
+  ];
+}
