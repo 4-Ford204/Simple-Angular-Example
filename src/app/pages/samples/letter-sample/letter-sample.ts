1bab7fd63a287f71eb90bdb00f14db0d67878ba8
Lê Tự Huỳnh Duy <huynhduy20042002@gmail.com>
2025-11-04 17:11:07 +0700

    Refactor sandbox structure and add letter sample/component
    Renamed 'sandbox' directories to 'sandboxes' for consistency and moved related files. Added new 'letter-sample' page and 'letter-component' utility component with initial implementation and test. This improves organization and introduces a reusable letter component.

---
+++ src/app/pages/samples/letter-sample/letter-sample.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-letter-sample',
+  imports: [],
+  templateUrl: './letter-sample.html',
+  styleUrl: './letter-sample.css'
+})
+export class LetterSample {
+
+}
