cc6196fac83645142e52e865a7b8d8c0ed5316d7
Lê Tự Huỳnh Duy <huynhduy20042002@gmail.com>
2026-09-11 11:24:21 +0700

    Resume

--- src/app/pages/samples/resume-sample/resume-sample.ts
+++ src/app/pages/samples/resume-sample/resume-sample.ts
@@ -8,9 +8,14 @@ import { ResumeComponent } from '../../../shared/utility-components/resume-compo
   styleUrl: './resume-sample.css',
 })
 export class ResumeSample {
-  image = 'https://www.ffcollectibles.com.au/cdn/shop/products/WARHEADSTUDIOREDROCLUFFY_1.jpg';
+  image =
+    'https://www.ffcollectibles.com.au/cdn/shop/files/AOSTUDIO_ONEPIECETHREEBROTHERSSERIES2.LUFFY_1.jpg?v=1684158397&width=493';
   name = 'Lê Tự Huỳnh Duy';
   position = '.NET Developer';
+  mail = 'adminorh@gmail.com';
+  phone = ' 0905 xxx 204';
+  birthday = '00/00/0000';
+  address = 'Da Nang City, Vietnam';
   description =
     'Looking for a challenging position in a dynamic environment where I can apply my knowledge and skills for continuous improvement, contribute to the growth of the organization by developing scalable and efficient software solutions that meet client needs and enhance user experience.';
   technologies = [
--- src/app/shared/utility-components/resume-component/resume-component.ts
+++ src/app/shared/utility-components/resume-component/resume-component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { ImageComponent } from '../../ui-components/media/image-component/image-component';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-resume-component',
@@ -13,6 +14,12 @@ export class ResumeComponent {
   @Input() image: string | undefined;
   @Input() name: string | undefined;
   @Input() position: string | undefined;
+  @Input() mail: string | undefined;
+  @Input() phone: string | undefined;
+  @Input() birthday: string | undefined;
+  @Input() address: string | undefined;
   @Input() description: string | undefined;
   @Input() technologies: any[] | undefined;
+
+  PrimeIcons = PrimeIcons;
 }
